843fc7377fb0d5339fd92949f59bc166ec6d4656
Shaba9 <shaba92@gmail.com>
2019-01-07 15:30:46 -0800

    map tests for holes and does not include them

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 const map = (arr, fn) => {
   const mappedArr = [];
   for(let i = 0; i < arr.length; i++) {
-    mappedArr.push(fn(arr[i]));
+    arr.hasOwnProperty(i) ? mappedArr.push(fn(arr[i])) : null;
   }
   return mappedArr;
 };
