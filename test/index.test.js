65802e9cece8fb2b2b1892e004c2681ae0ac0540
Kate Dameron <katerj33@gmail.com>
2019-01-07 16:23:47 -0800

    every function is passing test

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,8 @@ const {
     map,
     filter,
     findIndex,
-    reduce
+    reduce,
+    every
 } = require('../lib/index');
 
 describe('array methods', () => {
@@ -12,7 +13,7 @@ describe('array methods', () => {
                 const mapping = [1, 2, 3, 4, 5, 6].map(ele => {
                 return ele + 2;
                 });
-                
+
                 expect(mapping).toEqual([3, 4, 5, 6, 7, 8]);
             });
         });
@@ -42,16 +43,25 @@ describe('array methods', () => {
 
                 expect(reducing).toEqual(10);
             });
+        });
+        describe('every', () => {
+            it('will return a value of true', () => {
+                const every = [6, 7, 8].every(ele => {
+                    return ele > 5;
+                });
+
+                expect(every).toEqual(true);
+            });
         }); 
     });
     describe('implimentation', () => {
         describe('map', () => {
             it('can double every item in an array skipping holes', () => {
-                const results = map([2, 4, 6], ele => {
+                const results = map([2, 4, , 6], ele => {
                     return ele * 2;
                 });
 
-                expect(results).toEqual([4, 8, 12]);  
+                expect(results).toEqual([4, 8, , 12]);  
             });
         });
         describe('filter', () => {
@@ -81,5 +91,13 @@ describe('array methods', () => {
                 expect(results).toEqual(10);
             });
         });
+        describe('every', () => {
+            it('will return a value of true', () => {
+                const results = every([6, 7, , 8], ele => {
+                    return ele > 5;
+                })
+                expect(results).toEqual(true);
+            })
+        })
     });
 });
