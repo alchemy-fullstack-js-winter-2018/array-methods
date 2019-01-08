b3c83f6db12c6db39497448681c67c9064dc64eb
pickuppatterns <pickup.patterns@gmail.com>
2019-01-08 04:52:27 -0800

    updated holes in array for reduce method

--- test/index.test.js
+++ test/index.test.js
@@ -107,6 +107,11 @@ describe('array methods', () => {
 
                     expect(sum).toEqual(6);
                 });
+                it('can sum an array of numbers together & skip holes', () => {
+                    const sum = reduce([1, , 2, , 3], (acc, el) => {
+                        return acc + el;
+                    });
+                })
             });
             describe('findIndex', () => {
                 it('has to be a greater number in the array', () => {
