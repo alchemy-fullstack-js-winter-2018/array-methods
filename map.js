61c995c07e9cd69a942d37b98923bb7d82c65da9
Shaba9 <shaba92@gmail.com>
2019-01-07 16:18:29 -0800

    holes are kept as holes

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 const map = (arr, fn) => {
   const mappedArr = [];
   for(let i = 0; i < arr.length; i++) {
-    arr.hasOwnProperty(i) ? mappedArr.push(fn(arr[i])) : null;
+    arr.hasOwnProperty(i) ? mappedArr.push(fn(arr[i])) : mappedArr.push(arr[i]);
   }
   return mappedArr;
 };
