48018bfa7fc1f5ffd83cbe5bf66c717ed13f2c74
Shaba9 <shaba92@gmail.com>
2019-01-07 16:15:33 -0800

    reduce initialVal option added

--- reduce.js
+++ reduce.js
@@ -1,9 +1,9 @@
-const reduce = (arr, fn) => {
-  let total = arr[0];
+const reduce = (arr, fn, initialVal) => {
+  let total = initialVal || arr[0];
   for(let i = 0; i < arr.length - 1; i++) {
     total = fn(total, arr[i + 1]);
   }
-
+  initialVal ? total = fn(total, arr[0]) : null;
   return total;
 };
 
