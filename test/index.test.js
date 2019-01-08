665cfe874a2bdb4c8fc06060280ab4c8611ff9a0
Masmics <cherlocker3113@gmail.com>
2019-01-07 16:05:54 -0800

    map, filter, reduce working, findIndex IP

--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,52 @@
-const { map } = require('../lib/index');
+const { map, filter, reduce } = require('../lib/index');
 
 describe('array methods', () => {
-    describe('assumptions', () => {
-        describe('map', () =>  {
-            it('doubles each item in an array', () => {
-                const doubled = [2, 4, 6].map(ele => {
-                    return ele * 2;
-                });
-                expect(doubled).toEqual([4, 8, 12])
-            });
+  describe('assumptions', () => {
+    describe('map', () =>  {
+      it('doubles each item in an array', () => {
+        const doubled = [2, 4, 6].map(ele => {
+          return ele * 2;
         });
+        expect(doubled).toEqual([4, 8, 12]);
+      });
     });
+  });
 
-    describe('implementation', () => {
-        describe('map', () => {
-            it('can double every item in an array', () => {
-                const results = map([2, 4, 6]), ele => {
-                    return ele * 2;
-                };
-                expect(results).toEqual([4, 8, 12]);
-            };
+  describe('implementation', () => {
+    describe('map', () => {
+      it('can double every item in an array', () => {
+        const results = map([2, 4, 6], ele => {
+          return ele * 2;
         });
+        expect(results).toEqual([4, 8, 12]);
+      });
     });
+    describe('filter', () => {
+      it('can filter out odds', () => {
+        const evens = filter([1, 2, 3, 4], ele => {
+          return ele % 2 === 0;
+        });
+        expect(evens).toEqual([2, 4]);
+      });
+    });
+    // describe('findIndex', () => {
+    //   it('returns the item at the given index', () => {
+    //     const indexOf = findIndex([1, 3, 6, 9], ele => {
+    //       return ele[i];
+    //     });
+    //     expect(indexOf).toEqual()
+    //   });
+    // });
+
+    
+
+    describe('reduce', () => {
+      it('reduces to one number', () => {
+        const sum = reduce([2, 4], ele => {
+          return ele[0] + ele[1];
+        });
+        expect(sum).toEqual(6);
+      });
+    });
+  }); 
 }); 
