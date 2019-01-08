30ca2ed542d77e64c05fca1191f5dbd2f2bb033e
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-07 16:28:54 -0800

    asdf

---
+++ tests/index.test.js
@@ -0,0 +1,121 @@
+const {
+  map,
+  filter,
+  findIndex,
+  reduce
+} = require('../lib/index');
+
+describe('array methods', () => {
+
+  /////////////////
+  // ASSUMPTIONS //
+  /////////////////
+  describe('assumptions', () => {
+
+    describe('map', () => {
+      it('doubles each item in an array', () => {
+        const doubled = [2, 4, 6].map(ele => {
+          return ele * 2;
+        });
+
+        expect(doubled).toEqual([4, 8, 12]);
+      });
+
+      it('can double every item and skips holes', () => {
+        const arr = [2, 4, , 6];
+        const results = arr.map(ele => {
+          return 2 * ele;
+        });
+        expect(results).toEqual([4, 8, , 12]);
+      });
+    });
+
+    describe('filter', () => {
+      it('finds all indices that are less than 5', () => {
+        const lessThanFive = [2, 4, 6, 7].filter(ele => {
+          return ele < 5;
+        });
+
+        expect(lessThanFive).toEqual([2, 4]);
+      });
+    });
+
+    describe('findIndex', () => {
+      it('finds the index of the first element that is less than two', () => {
+        const lessThanTwo = [4, 6, 3, 1, 5, 0, -2].findIndex(ele => {
+          return ele < 2;
+        });
+
+        expect(lessThanTwo).toEqual(3);
+      });
+    });
+
+    describe('reduce', () => {
+      it('sums all the numbers in an array', () => {
+        const summedNumbers = [1, 2, 3, 4, 5].reduce((acc, ele) => {
+          return acc += ele;
+        });
+
+        expect(summedNumbers).toEqual(15);
+      });
+    });
+  });
+
+  ////////////////////
+  // IMPLEMENTATION //
+  ////////////////////
+  describe('implementation', () => {
+
+    describe('map', () => {
+      it('can double every item in an array', () => {
+        const results = map([2, 4, 6], ele => {
+          return ele * 2;
+        });
+
+        expect(results).toEqual([4, 8, 12]);
+      });
+
+      it('can double every item and skip holes', () => {
+        const results = map([2, 4, , 6], ele => {
+          return ele * 2;
+        });
+
+        expect(results).toEqual([4, 8, , 12]);
+      });
+
+
+    });
+
+    describe('filter', () => {
+      it('finds all indices that are less than 5', () => {
+        const lessThanFive = filter([2, 4, 6, 7], ele => {
+          return ele < 5;
+        });
+
+        expect(lessThanFive).toEqual([2, 4]);
+      });
+    });
+
+    describe('findIndex', () => {
+      it('finds the index of the first element that is less than two', () => {
+        const lessThanTwo = findIndex([4, 6, 3, 1, 5, 0, -2], ele => {
+          return ele < 2;
+        });
+
+        expect(lessThanTwo).toEqual(3);
+      });
+    });
+
+    describe('reduce', () => {
+      it('sums all the numbers in an array', () => {
+        const summedNumbers = reduce([1, 2, 3, 4, 5], (acc, ele) => {
+          return acc += ele;
+        }, 0);
+
+        expect(summedNumbers).toEqual(15);
+      });
+    });
+
+    
+  });
+});
