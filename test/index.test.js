afc8fc22faa301a4d82c3b1e5b0b4eb71aa759b2
Aaron Dennis <AMDennis1987@gmail.com>
2019-01-08 10:50:54 -0800

    Fixed map with holes test

--- test/index.test.js
+++ test/index.test.js
@@ -8,10 +8,10 @@ describe('array methods', () => {
   describe('assumptions', () => {
     describe('map', () => {
       it('double each item in array', () => {
-        const double = [1, 2, 3, 4].map(ele => {
+        const double = [1, 2, , 3, 4].map(ele => {
           return ele * 2;
         });
-        expect(double).toEqual([2, 4, 6, 8]);
+        expect(double).toEqual([2, 4, , 6, 8]);
       });
     });
   });
