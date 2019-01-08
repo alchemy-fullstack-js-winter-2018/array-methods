06ebd7319ac6feb7b1da633cf46433fea744d723
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-01-07 16:34:14 -0800

    every function works

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,9 @@
 const { 
   map,
   filter,
-  findIndex
+  findIndex,
+  reduce,
+  every
 } = require('../lib/index');
 
 describe('array methods', () => {
@@ -32,37 +34,79 @@ describe('array methods', () => {
         });
         expect(findIndex).toEqual(2);
       });
-
     });
 
-    describe('implementation', () => {
-      describe('map', () => {
-        it('can double numbers in every array', () => {
-          const results = map([2, 4, 6], ele => {
-            return ele * 2;
-          });
+    describe('reduce', () => [
+      it('returns the sum of the array', () => {
+        const sum = [1, 2, 3].reduce((acc, ele) => {
+          return acc + ele;
+        }, 0);
+        expect(sum).toEqual(6);
+      })
+    ]);
 
-          expect(results).toEqual([4, 8, 12]);
+    describe('every', () => {
+      it('returns true if every item in the array is even', () => {
+        const every = [2, 4, 6, 8].every(ele => {
+          return ele % 2 === 0;
         });
+        expect(every).toEqual(true);
       });
+    });
+  });
 
-      describe('filter', () => {
-        it('can return numbers less than 3 in every array', () => {
-          const filterResults = filter([1, 2, 3, 4], ele => {
-            return ele < 3 ;
-          });
-          expect(filterResults).toEqual([1, 2]);
+  describe('implementation', () => {
+    describe('map', () => {
+      it('can double numbers in every array', () => {
+        const results = map([2, 4, , 6], ele => {
+          return ele * 2;
         });
+
+        expect(results).toEqual([4, 8, 12]);
       });
+    });
+
+    describe('filter', () => {
+      it('can return numbers less than 3 in every array', () => {
+        const filterResults = filter([1, 2, 3, 4], ele => {
+          return ele < 3 ;
+        });
+        expect(filterResults).toEqual([1, 2]);
+      });
+    });
       
-      describe('findIndex', () => {
-        it('can return first even number in every array', () => {
-          const indexResults = findIndex([3, 7, 6, 4], ele => {
-            return ele % 2 === 0;
-          });
-          expect(indexResults).toEqual(2);
+    describe('findIndex', () => {
+      it('can return first even number in every array', () => {
+        const indexResults = findIndex([3, 7, 6, 4], ele => {
+          return ele % 2 === 0;
         });
+        expect(indexResults).toEqual(2);
       });
     });
+
+    describe('reduceMethod', () => {
+      it('can return the sum of the each array', () => {
+        const sum = reduce([1, 2, 3, 4], (acc, ele) => { 
+          return acc + ele;
+        }, 0);
+        expect(sum).toEqual(10);
+      });
+      it('can sum an array with no accumulator', () => {
+        const sum = reduce([1, 2, 3, 4], (acc, ele) => {
+          return acc + ele;
+        });
+        expect(sum).toEqual(10);
+      });
+    });
+
+    describe('every', () => {
+      it('can return true for all array with even numbers', () => {
+        const everyResults = every([2, 4, 6, 8], ele => {
+          return ele % 2 === 0;
+        });
+        expect(everyResults).toEqual(true);
+      });
+    });
+
   });
 });
