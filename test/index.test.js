37a5e52a88d9c70b1b0c2098e6bb9a44c40bf0e6
Sophie Graham <sophiergraham1@gmail.com>
2019-01-15 09:52:22 -0800

    added and passed map implementation

--- test/index.test.js
+++ test/index.test.js
@@ -16,43 +16,67 @@ describe('array methods', () => {
         });
         expect(double).toEqual([2, 4, 6, 8]);
       });
-    });
-  });
-
-  describe('filter', () => {
-    it('takes out odd numbers', () => {
-      const filtered = [1, 2, 3].filter(ele => {
-        return ele % 2 === 0;
+      
+      it('double the numbers and skip holes', () => {
+        const arr = [1, 2, , 4];
+        const results = arr.map(ele => {
+          return ele * 2;
+        });
+        expect (results).toEqual([2, 4, ,8]);
       });
-      expect(filtered).toEqual([2]);
     });
-  });
-
-  describe('findIndex', () => {
-    it('finds first index greater than 5', () => {
-      const index = [1, 2, 3, 4, 5, 6, 7, 8, 9].findIndex(ele => {
-        return ele > 5;
+      
+    describe('filter', () => {
+      it('takes out odd numbers', () => {
+        const filtered = [1, 2, 3].filter(ele => {
+          return ele % 2 === 0;
+        });
+        expect(filtered).toEqual([2]);
       });
-      expect(index).toEqual(5);
     });
-  });
 
-  describe('reduce', () => {
-    it('can get the sum of an array', () => {
-      const reduced = [1, 2, 3].reduce((acc, ele) => {
-        return acc += ele;
-      }, 0);
-      expect(reduced).toEqual(6);
-    });
-  });
-
-  describe('every', () => {
-    it('returns true if all items are greater than 0', () => {
-      const results = [1, 2, 3].every(ele => {
-        return ele > 0;
+    describe('findIndex', () => {
+      it('finds first index greater than 5', () => {
+        const index = [1, 2, 3, 4, 5, 6, 7, 8, 9].findIndex(ele => {
+          return ele > 5;
+        });
+        expect(index).toEqual(5);
       });
-      expect(results).toEqual(true);
+    });
+
+    describe('reduce', () => {
+      it('can get the sum of an array', () => {
+        const reduced = [1, 2, 3].reduce((acc, ele) => {
+          return acc += ele;
+        }, 0);
+        expect(reduced).toEqual(6);
+      });
+    });
+
+    describe('every', () => {
+      it('returns true if all items are greater than 0', () => {
+        const results = [1, 2, 3].every(ele => {
+          return ele > 0;
+        });
+        expect(results).toEqual(true);
+      });
     });
   });
+});
 
+describe('implementation', () => {
+  describe('map', () => {
+    it('can double every item in an array', () => {
+      const results = map([2, 4, 6], ele => {
+        return ele * 2;
+      });
+      expect(results).toEqual([4, 8, 12]);
+    });
+  });
+  it('doubles every item and skip holes', () => {
+    const results = map([1, 2, , 3], ele => {
+      return ele * 2;
+    });
+    expect(results).toEqual([2, 4, , 6]);
+  });
 });
